995054cc37d860258fda0f2c0247df8839a99bc5
Nisha Bankar <nishab2607@gmail.com>
2025-08-02 16:55:43 +0530

    add remaning educatordashboard pages code

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Outlet} from 'react-router-dom'
 import Navbar from '../../components/educator/Navbar'
 import Sidebar from '../../components/educator/Sidebar'
+import Footer from '../../components/educator/Footer'
 
 const Educator = () => {
     return (
@@ -13,7 +14,10 @@ const Educator = () => {
                    {<Outlet/>}
                 </div>
             </div>
+            <Footer/>
         </div>
+
+
     )
 }
 
