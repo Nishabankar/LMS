8a28d552513b051af29287ec354fed4f35eff858
Nisha Bankar <nishab2607@gmail.com>
2025-08-23 19:16:31 +0530

    another controller code added

--- server/controllers/educatorControllers.js
+++ server/controllers/educatorControllers.js
@@ -1,6 +1,8 @@
-import { clerkClient } from "@clerk/express";
+import { clerkClient} from "@clerk/express";
 import {v2 as cloudinary} from 'cloudinary'
 import Course from "../models/Course.js";
+import { Purchase } from "../models/Purchase.js";
+import User from "../models/User.js";
 
 
 // update role to educator
@@ -46,5 +48,93 @@ export const addCourse = async (req, res) => {
         res.json({success: true, message: 'Course Added'})
     } catch (error) {
      res.json({success: false, message:error.message})
+    }
+}
+
+// Get Educator Courses
+
+export const getEducatorCourse = async ( req, res ) => {
+    try {
+        const educator = req.auth.userId
+        const courses = await Course.find( { educator } )
+        res.json({success: true, courses})
+    } catch ( error ) {
+        res.json({success: false, message: error.message})
+
+    }
+}
+
+
+// Get Educator Dashboard Data (Total Earning, Enrolled Students, No of Courses)
+
+export const educatorDashboardData = async (req, res) => {
+    try {
+
+        const educator = req.auth.userId;
+        const courses = await Course.find( { educator } );
+        const totalCourses = courses.length;
+
+        const courseIds = courses.map( course => course_id );
+
+        // Calculate total earnings from purchase
+        const purchases = await Purchase.fing( {
+            courseId: { $in: courseIds },
+            status: 'completed'
+        } );
+
+        const totalEarnings = purchases.reduce( ( sum, purchase ) => sum + purchase.amount, 0 );
+
+        // Collect unique enrolled student IDs with their course titles
+
+        const enrolledStudentsData = [];
+        for ( const course of courses ) {
+            const students = await User.find( {
+                _id: { $in: course.enrolledStudents }
+            }, 'name imageUrl' );
+
+            students.forEach(student => {
+                enrolledStudentsData.push( {
+                    courseTitle: course.courseTitle,
+                    student
+                } );
+            });
+        }
+
+        res.json( {
+            success: true, dashboardData: {
+            totalEarnings, enrolledStudentsData, totalCourses
+        }})
+    } catch (error) {
+        res.json( { success: false, message: error.message } );
+    }
+}
+
+
+// Get Enrolled Students Data with Purchase Data
+
+export const getEnrolledStudentsData = async ( req, res ) => {
+    try {
+        const educator = req.auth.userId;
+        const courses = await Course.find( { educator } );
+        const courseIds = courses.map( course => course_id );
+
+        const purchases = await Purchase.find( {
+            courseId: { $in: courseIds },
+            status: 'completed'
+
+        } ).populate( 'userId', 'name imageUrl' ).populate( 'courseId', 'courseTitle' )
+
+        const enrolledStudents = purchases.map( purchase => ( {
+
+            student: purchase.userId,
+            courseTitle: purchase.courseId.courseTitle,
+            purchaseData: purchase.createdAt
+        } ) );
+        res.json( { success: true, enrolledStudents } )
+
+    } catch ( error ) {
+        res.json( { success: false, message: error.message } );
+
+
     }
 }
--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addCourse, updateRoleToEducator } from '../controllers/educatorControllers.js'
+import { addCourse, educatorDashboardData, getEducatorCourse, getEnrolledStudentsData, updateRoleToEducator } from '../controllers/educatorControllers.js'
 import { requireAuth } from '@clerk/express'
 import { protectEducator } from '../middlewares/authMiddleware.js'
 import upload from '../configs/multer.js'
@@ -8,6 +8,9 @@ const educatorRouter = express.Router()
 
 // Add Educator Role (Protected)
 educatorRouter.get( '/update-role', requireAuth(), updateRoleToEducator )
-educatorRouter.post('/add-course',requireAuth(), protectEducator, upload.single('image'), addCourse)
+educatorRouter.post( '/add-course', requireAuth(), protectEducator, upload.single( 'image' ), addCourse )
+educatorRouter.get('/courses', protectEducator, getEducatorCourse)
+educatorRouter.get('/dashboard', protectEducator, educatorDashboardData)
+educatorRouter.get('/enrolled-students', protectEducator, getEnrolledStudentsData)
 
 export default educatorRouter
